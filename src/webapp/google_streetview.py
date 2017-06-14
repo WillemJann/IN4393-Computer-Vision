c438ec8864544fbc49a0aad96fe75b7f93c520a8
Willem-Jan Meerkerk <willemjanmeerkerk@hotmail.com>
2017-06-14 11:30:12 +0200

    Added 'save image' button

--- src/webapp/google_streetview.py
+++ src/webapp/google_streetview.py
@@ -1,11 +1,14 @@
 import cv2
+import glob
 import numpy as np
+import os
 import urllib
 
 API_KEY = 'AIzaSyCW38Y73-X6r2IVg6GHexRgu-X07uIlHGQ'
 IMAGE_SIZE = '640x480'
+IMAGE_FOLDER = '../../data/streetview_images/'
 
-def get_streetview_image(location, heading, pitch, fov):
+def download_image(location, heading, pitch, fov):
     url = 'https://maps.googleapis.com/maps/api/streetview?key=%s&size=%s&location=%s&heading=%s&pitch=%s&fov=%s' % (API_KEY, IMAGE_SIZE, location, heading, pitch, fov);
     
     # Download the image, convert it to a numpy array
@@ -13,5 +16,29 @@ def get_streetview_image(location, heading, pitch, fov):
     image = np.asarray(bytearray(response.read()), dtype='uint8')
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     
+    return image
+
+def get_image(location, heading, pitch, fov):
+    image = download_image(location, heading, pitch, fov)
+    
+    # Return image data
     _, imagedata = cv2.imencode('.jpg', image)
     return imagedata
+
+def save_image(location, heading, pitch, fov):
+    image = download_image(location, heading, pitch, fov)
+    
+    # Determine image number
+    image_number = 1
+    files = glob.glob(IMAGE_FOLDER + '*.jpg')
+    
+    if len(files) > 0:
+        last_file = max(files)
+        file_name = os.path.basename(last_file)
+        file_number = int(os.path.splitext(file_name)[0])
+        image_number += file_number
+    
+    # Save image
+    cv2.imwrite(IMAGE_FOLDER + '%06d.jpg' % image_number, image)
+    
+    return True
--- src/webapp/server.py
+++ src/webapp/server.py
@@ -1,9 +1,11 @@
 from io import BytesIO
+
 from flask import Flask, request, jsonify
 from flask.helpers import send_file
 
 from exception import WebAppException, MISSING_ARGUMENTS
-from google_streetview import get_streetview_image
+import google_streetview
+
 
 # The main application:
 app = Flask(__name__, static_url_path="", static_folder="")
@@ -21,8 +23,8 @@ def index():
     return app.send_static_file('index.html')
 
 # API endpoint for getting images from Google Streetview
-@app.route('/api/streetview_image')
-def streetview_image():
+@app.route('/api/get_streetview_image')
+def get_streetview_image():
     try:
         location = request.args['location']
         heading = request.args['heading']
@@ -31,9 +33,27 @@ def streetview_image():
     except:
         raise WebAppException(error_code=MISSING_ARGUMENTS)
     
-    imagedata = get_streetview_image(location, heading, pitch, fov)
+    imagedata = google_streetview.get_image(location, heading, pitch, fov)
     
     return send_file(BytesIO(imagedata), 'image/jpeg')
 
+@app.route('/api/save_streetview_image')
+def save_streetview_image():
+    try:
+        location = request.args['location']
+        heading = request.args['heading']
+        pitch = request.args['pitch']
+        fov = request.args['fov']
+    except:
+        raise WebAppException(error_code=MISSING_ARGUMENTS)
+    
+    google_streetview.save_image(location, heading, pitch, fov)
+    
+    return ''
+
 # API endpoint for road sign detection on Google Streetview
 # TODO
+
+# Run application
+if __name__ == "__main__":
+    app.run(debug = True)
