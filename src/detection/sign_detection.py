cf7d9abb26885be48b53c3d093cd4b3ffd7581e7
Willem-Jan Meerkerk <willemjanmeerkerk@hotmail.com>
2017-06-22 15:29:26 +0200

    Implemented sign segmentation and circle detection in webapp

--- src/detection/sign_detection.py
+++ src/detection/sign_detection.py
@@ -1,14 +1,23 @@
-import io
-import skimage.io
+from io import BytesIO
+
+from find_patches import binary_filtering, extract_ROIs
+from find_circles import detect_circles, draw_circles
 
 def detect_signs(image):
-    # TODO: implement sign detection
+    # Find patches
+    segmented_image = binary_filtering(image)
+    patches = extract_ROIs(segmented_image)
     
-    # Return results
-    output = io.BytesIO()
-    skimage.io.imsave(output, image, plugin='pil', format_str='jpeg')
-    output.seek(0)
+    # Detect circles
+    circles = detect_circles(image, patches)
     
+    # Classify signs
     recognized_signs = {}
     
+    # Generate output image
+    output = BytesIO()
+    draw_circles(image, circles, output)
+    output.seek(0)
+    
+    # Return output image and classification result
     return output, recognized_signs
