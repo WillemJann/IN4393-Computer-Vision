5df6958a0fc77befad18d897a14f40dccb114568
Willem-Jan Meerkerk <willemjanmeerkerk@hotmail.com>
2017-06-29 16:18:59 +0200

    Implemented pre-defined locations

--- src/webapp/server.py
+++ src/webapp/server.py
@@ -1,4 +1,6 @@
-from base64 import b64encode
+import base64
+import csv
+
 from flask import Flask, request, jsonify
 from flask.helpers import send_file
 
@@ -37,20 +39,6 @@ def get_streetview_image():
     
     return send_file(imagedata, 'image/jpeg')
 
-@app.route('/api/save_streetview_image')
-def save_streetview_image():
-    try:
-        location = request.args['location']
-        heading = request.args['heading']
-        pitch = request.args['pitch']
-        fov = request.args['fov']
-    except:
-        raise WebAppException(error_code=MISSING_ARGUMENTS)
-    
-    google_streetview.save_image(location, heading, pitch, fov)
-    
-    return ''
-
 # API endpoint for road sign detection on Google Streetview
 @app.route('/api/roadsign_detection')
 def roadsign_detection():
@@ -66,8 +54,49 @@ def roadsign_detection():
     
     output, recognized_signs = detect_signs(image)
     
-    return jsonify(image = b64encode(output.read()), classification = recognized_signs)
+    return jsonify(image = base64.b64encode(output.read()), classification = recognized_signs)
 
+# API endpoint for saving a Google Streetview image
+@app.route('/api/save_streetview_image')
+def save_streetview_image():
+    try:
+        location = request.args['location']
+        heading = request.args['heading']
+        pitch = request.args['pitch']
+        fov = request.args['fov']
+    except:
+        raise WebAppException(error_code=MISSING_ARGUMENTS)
+    
+    google_streetview.save_image(location, heading, pitch, fov)
+    
+    return ''
+
+# API endpoint for retrieving the list of pre-defined locations
+@app.route('/api/load_predefined_locations')
+def load_predefined_locations():
+    with open('../../data/predefined_locations.csv') as file:
+        reader = csv.DictReader(file, quoting=csv.QUOTE_NONNUMERIC, fieldnames=('lat', 'lng', 'heading', 'pitch', 'zoom'))
+        locations = [row for row in reader]
+        
+    return jsonify(locations)
+
+# API endpoint for saving a new pre-defined location
+@app.route('/api/save_predefined_location')
+def save_predefined_location():
+    try:
+        lat = request.args['lat']
+        lng = request.args['lng']
+        heading = request.args['heading']
+        pitch = request.args['pitch']
+        zoom = request.args['zoom']
+    except:
+        raise WebAppException(error_code=MISSING_ARGUMENTS)
+    
+    with open('../../data/predefined_locations.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([lat, lng, heading, pitch, zoom])
+    
+    return ''
 
 # Run application
 if __name__ == "__main__":
