98fb9b81d8ec31fc89d2fd3b5411025417e91560
pbrand <p.brand@student.tudelft.nl>
2017-06-30 22:08:23 +0200

    Implemented creation of hog cells and histograms

--- src/features/our_hog.py
+++ src/features/our_hog.py
@@ -24,11 +24,57 @@ def calculate_gradient(image):
 
 
 def create_histogram(angles, weights, nr_of_bins=9, hist_range=(0, 180)):
-    pass
+    # Define the bin size and the lower bounds of each bin in the histogram
+    bin_size = hist_range[1] / nr_of_bins
+    bin_edges = range(hist_range[0], hist_range[1] + 1, bin_size)
+
+    # Create a 1D histogram of size: nr_of_bins
+    histogram = np.zeros(nr_of_bins, dtype=np.double)
+    # The idea is that a histogram is created of all gradient values of a cell as this is a form of quantization.
+    # The angles of the gradient define to which 2 bins this gradient belongs to.
+    # The magnitudes of the gradient are the weights of how much that gradient contributes to the two bins.
+    # This magnitude as weight is linearly interpolated between the two bins.
+
+    # For each bin:
+    # - Calculate which indices of the angles list falls into this bin
+    # - For each index:
+    #    - Get the angle at position 'index' in the angles list
+    #    - Get the corresponding weight
+    #    - Linearly interpolate the weight between the two neighbouring bins
+    #    - Note that an angle of for instance 177 degrees contributes both to the bin 170-180 and 0-10,
+    #      so bin indices should be defined circular
+    for k in range(len(bin_edges)):
+        indices_in_range = np.where(np.logical_and(angles >= bin_edges[k], angles < bin_edges[k] + bin_size))[0]
+        for index in indices_in_range:
+            angle = angles[index]
+            weight = weights[index]
+
+            diff = (angle - np.double(bin_edges[k]))
+            histogram[k] += weight * (1 - (diff / np.double(bin_size)))
+            histogram[(k + 1) % nr_of_bins] += weight * (diff / np.double(bin_size))
+    return histogram
 
 
 def create_hog_cells(matrix, cell_shape=(8,8)):
-    pass
+    # Calculate the number of cells that fit in the image.
+    # If the image dimensions are not a multiple of the cell_shape, the maximum amount of
+    # fully filled cells will be taken, the boundaries of the image where no complete cell fits,
+    # will be discarded.
+    nr_cells = (np.uint8(np.floor(np.float(matrix.shape[0]) / (np.float(cell_shape[0])))),
+                 np.uint8(np.floor(np.float(matrix.shape[0]) / (np.float(cell_shape[0])))))
+    # Create the output matrix which contains the image when split into cells
+    cells = np.zeros((nr_cells[0], nr_cells[1], cell_shape[0], cell_shape[1]), dtype=np.double)
+    # For each cell in the output matrix, copy the corresponding image values into the output matrix
+    for row in range(nr_cells[0]):
+        for col in range(nr_cells[1]):
+            row_start = row * cell_shape[0]
+            row_end = row_start + cell_shape[0]
+            col_start = col * cell_shape[1]
+            col_end = col_start + cell_shape[1]
+
+            cell = matrix[row_start:row_end, col_start:col_end]
+            cells[row, col, :, :] = cell
+    return cells
 
 
 def create_hog_blocks(cells, block_shape=(3,3)):
